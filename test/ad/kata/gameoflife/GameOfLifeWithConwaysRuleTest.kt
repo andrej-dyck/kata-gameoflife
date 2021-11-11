5d6fc2b68874b2ee7b695c190b7d574eabdb3b57
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2021-11-11 21:27:21 +0100

    Add (disabled) acceptance tests for well known patterns

--- test/ad/kata/gameoflife/GameOfLifeWithConwaysRuleTest.kt
+++ test/ad/kata/gameoflife/GameOfLifeWithConwaysRuleTest.kt
@@ -1,6 +1,7 @@
 package ad.kata.gameoflife
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -75,5 +76,335 @@ class GameOfLifeWithConwaysRuleTest {
                 simulateWith(board).take(cycles).toList()
             ).containsOnly(board)
         }
+    }
+
+    @Nested
+    inner class StablePatterns {
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `small rotated-L pattern must result in stable square in 1 iteration`() {
+            val givenBoard = boardFromString(
+                ".##.",
+                ".#.."
+            )
+            val stableBoard = boardFromString(
+                ".##.",
+                ".##."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                givenBoard,
+                stableBoard,
+                stableBoard
+            )
+        }
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `large rotated-L pattern must result in stable circle in 3 iterations`() {
+            val givenBoard = boardFromString(
+                ".....",
+                ".###.",
+                ".#...",
+                "....."
+            )
+            val stableBoard = boardFromString(
+                ".##..",
+                "#..#.",
+                ".##..",
+                "....."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                givenBoard,
+                boardFromString(
+                    "..#..",
+                    ".##..",
+                    ".#...",
+                    "....."
+                ),
+                boardFromString(
+                    ".##..",
+                    ".##..",
+                    ".##..",
+                    "....."
+                ),
+                stableBoard,
+                stableBoard
+            )
+        }
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `large I pattern must result in stable oval in 3 iterations`() {
+            val givenBoard = boardFromString(
+                "...#...",
+                "...#...",
+                "...#...",
+                "...#..."
+            )
+            val stableBoard = boardFromString(
+                "...#...",
+                "..#.#..",
+                "..#.#..",
+                "...#..."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                givenBoard,
+                boardFromString(
+                    ".......",
+                    "..###..",
+                    "..###..",
+                    "......."
+                ),
+                stableBoard,
+                stableBoard
+            )
+        }
+    }
+
+    @Nested
+    inner class DyingPatterns {
+
+        private val allCellsAreDeadBoard = boardFromString()
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `i pattern must result in all dead in 2 iterations`() =
+            `assertThat this is the simulated sequence of boards`(
+                boardFromString(
+                    "..#.",
+                    ".#..",
+                    ".#.."
+                ),
+                boardFromString(
+                    "....",
+                    ".##.",
+                    "...."
+                ),
+                allCellsAreDeadBoard
+            )
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `diagonal line pattern must result in all dead in 2 iterations`() =
+            `assertThat this is the simulated sequence of boards`(
+                boardFromString(
+                    "...#.",
+                    "..#..",
+                    ".#..."
+                ),
+                boardFromString(
+                    ".....",
+                    "..#..",
+                    "....."
+                ),
+                allCellsAreDeadBoard
+            )
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `rotated-i pattern must result in all dead in 5 iterations`() =
+            `assertThat this is the simulated sequence of boards`(
+                boardFromString(
+                    "......",
+                    ".#....",
+                    "..###.",
+                    "......"
+                ),
+                boardFromString(
+                    "......",
+                    "..##..",
+                    "..##..",
+                    "...#.."
+                ),
+                boardFromString(
+                    "......",
+                    "..##..",
+                    "....#.",
+                    "..##.."
+                ),
+                boardFromString(
+                    "......",
+                    "...#..",
+                    "....#.",
+                    "...#.."
+                ),
+                boardFromString(
+                    "......",
+                    "......",
+                    "...##.",
+                    "......"
+                ),
+                allCellsAreDeadBoard
+            )
+    }
+
+    @Nested
+    inner class OscillatingPatterns {
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `blinker pattern is a period 2 oscillator`() {
+            val seed = boardFromString(
+                "..#..",
+                "..#..",
+                "..#.."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                seed,
+                boardFromString(
+                    ".....",
+                    ".###.",
+                    "....."
+                ),
+                seed
+            )
+        }
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `clock pattern is a period 2 oscillator`() {
+            val seed = boardFromString(
+                "...#..",
+                ".##...",
+                "...##.",
+                "..#..."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                seed,
+                boardFromString(
+                    "..#...",
+                    "..#.#.",
+                    ".#.#..",
+                    "...#.."
+                ),
+                seed
+            )
+        }
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `pulsar pattern is a period 3 oscillator`() {
+            val seed = boardFromString(
+                ".................",
+                ".................",
+                "....###...###....",
+                ".................",
+                "..#....#.#....#..",
+                "..#....#.#....#..",
+                "..#....#.#....#..",
+                "....###...###....",
+                ".................",
+                "....###...###....",
+                "..#....#.#....#..",
+                "..#....#.#....#..",
+                "..#....#.#....#..",
+                ".................",
+                "....###...###....",
+                ".................",
+                ".................",
+                "................."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                seed,
+                boardFromString(
+                    ".................",
+                    ".....#.....#.....",
+                    ".....#.....#.....",
+                    ".....##...##.....",
+                    ".................",
+                    ".###..##.##..###.",
+                    "...#.#.#.#.#.#...",
+                    ".....##...##.....",
+                    ".................",
+                    ".....##...##.....",
+                    "...#.#.#.#.#.#...",
+                    ".###..##.##..###.",
+                    ".................",
+                    ".....##...##.....",
+                    ".....#.....#.....",
+                    ".....#.....#.....",
+                    ".................",
+                    "................."
+                ),
+                boardFromString(
+                    ".................",
+                    ".................",
+                    "....##.....##....",
+                    ".....##...##.....",
+                    "..#..#.#.#.#..#..",
+                    "..###.##.##.###..",
+                    "...#.#.#.#.#.#...",
+                    "....###...###....",
+                    ".................",
+                    "....###...###....",
+                    "...#.#.#.#.#.#...",
+                    "..###.##.##.###..",
+                    "..#..#.#.#.#..#..",
+                    ".....##...##.....",
+                    "....##.....##....",
+                    ".................",
+                    ".................",
+                    "................."
+                ),
+                seed
+            )
+        }
+
+        @Test
+        @Disabled("not yet implemented")
+        fun `glider is a moving period 4 oscillator`() {
+            val seed = boardFromString(
+                "..#....",
+                "...#...",
+                ".###...",
+                "......."
+            )
+
+            `assertThat this is the simulated sequence of boards`(
+                seed,
+                boardFromString(
+                    ".......",
+                    ".#.#...",
+                    "..##...",
+                    "..#...."
+                ),
+                boardFromString(
+                    ".......",
+                    "...#...",
+                    ".#.#...",
+                    "..##..."
+                ),
+                boardFromString(
+                    ".......",
+                    "..#....",
+                    "...##..",
+                    "..##..."
+                ),
+                boardFromString(
+                    ".......",
+                    "...#...",
+                    "....#..",
+                    "..###.."
+                )
+            )
+        }
+    }
+
+    private fun `assertThat this is the simulated sequence of boards`(
+        seededBoard: Board,
+        vararg expectedBoards: Board
+    ) {
+        assertThat(
+            simulateWith(seededBoard).take(expectedBoards.size + 1).toList()
+        ).containsExactlyElementsOf(
+            listOf(seededBoard) + expectedBoards
+        )
     }
 }
